7e65532ac49fb86cf42e389ae31509c3832cec59
rbarghou <ramsey.barghouti@gmail.com>
2014-12-29 22:42:10 -0500

    made svg logos

--- js/app.js
+++ js/app.js
@@ -2,38 +2,51 @@ angular
     .module('ramseybarghouticom', [])
     .controller('dataScienceSectionController', ['$scope', '$http',
         function ($scope, $http) {
-            $http.get('DataScienceBadgeData.json')
+            $http.get('data/DataScienceBadgeData.json')
                 .success(function (data, status, headers, config) {
-                    $scope.badges = data;
+                    $scope.badges = data.badges;
+                });
+    }])
+    .controller('developerSectionController', ['$scope', '$http',
+        function ($scope, $http) {
+            $http.get('data/DeveloperBadgeData.json')
+                .success(function (data, status, headers, config) {
+                    console.log(data);
+                    $scope.badges = data.badges;
                 });
     }])
     .directive('contact', function () {
         return {
-            templateUrl: 'contact.html'
+            templateUrl: 'templates/contact.html'
         };
     })
     .directive('about', function () {
         return {
-            templateUrl: 'about.html'
+            templateUrl: 'templates/about.html'
         }
     })
     .directive('datascience', function () {
         return {
-            templateUrl: 'datascience.html'
+            templateUrl: 'templates/datascience.html'
         }
     })
     .directive('developer', function () {
         return {
-            templateUrl: 'developer.html'
+            templateUrl: 'templates/developer.html'
         }
     })
     .directive('intro', function () {
         return {
-            templateUrl: 'intro.html'
+            templateUrl: 'templates/intro.html'
         };
     })
     .directive('copyright', function () {
         return {
-            templateUrl: 'copyright.html'
+            templateUrl: 'templates/copyright.html'
+        }
+    })
+    .directive('navigation', function () {
+        return {
+            templateUrl: 'templates/navigation.html'
         }
     });
--- js/grayscale.js
+++ js/grayscale.js
@@ -5,6 +5,8 @@
  */
 
 // jQuery to collapse the navbar on scroll
+
+
 $(window).scroll(function () {
     if ($(".navbar").offset().top > 50) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
