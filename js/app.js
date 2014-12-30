a4d673e060674705fbd20dd60c87054a7f10d47e
rbarghou <ramsey.barghouti@gmail.com>
2014-12-30 15:42:06 -0500

    migrated greyscale theme code into angular controller

--- js/app.js
+++ js/app.js
@@ -1,19 +1,51 @@
 angular
     .module('ramseybarghouticom', [])
+    .controller('navbarController', ['$scope',
+        function ($scope) {
+            // jQuery to collapse the navbar on scroll
+            $(window).scroll(function () {
+                if ($(".navbar").offset().top > 50) {
+                    $(".navbar-fixed-top").addClass("top-nav-collapse");
+                } else {
+                    $(".navbar-fixed-top").removeClass("top-nav-collapse");
+                }
+            });
+
+            // jQuery for page scrolling feature - requires jQuery Easing plugin
+            $(function () {
+                $('a.page-scroll').bind('click', function (event) {
+                    var $anchor = $(this);
+                    $('html, body').stop().animate({
+                        scrollTop: $($anchor.attr('href')).offset().top
+                    }, 1500, 'easeInOutExpo');
+                    event.preventDefault();
+                });
+            });
+
+            // Closes the Responsive Menu on Menu Item Click
+            $('.navbar-collapse ul li a').click(function () {
+                $('.navbar-toggle:visible').click();
+            });
+    }])
     .controller('dataScienceSectionController', ['$scope', '$http',
         function ($scope, $http) {
-            $http.get('data/DataScienceBadgeData.json')
-                .success(function (data, status, headers, config) {
-                    $scope.badges = data.badges;
-                });
+            $http.get('data/DataScienceBadgeData.json', {
+                headers: {
+                    'Content-Type': 'text/javascript'
+                }
+            }).success(function (data, status, headers, config) {
+                $scope.badges = data.badges;
+            });
     }])
     .controller('developerSectionController', ['$scope', '$http',
         function ($scope, $http) {
-            $http.get('data/DeveloperBadgeData.json')
-                .success(function (data, status, headers, config) {
-                    console.log(data);
-                    $scope.badges = data.badges;
-                });
+            $http.get('data/DeveloperBadgeData.json', {
+                headers: {
+                    'Content-Type': 'text/javascript'
+                }
+            }).success(function (data, status, headers, config) {
+                $scope.badges = data.badges;
+            });
     }])
     .directive('contact', function () {
         return {
--- js/grayscale.js
+++ js/grayscale.js
@@ -5,8 +5,6 @@
  */
 
 // jQuery to collapse the navbar on scroll
-
-
 $(window).scroll(function () {
     if ($(".navbar").offset().top > 50) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
